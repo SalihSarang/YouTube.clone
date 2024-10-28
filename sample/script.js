a8c40f96dc1cc11805fd0302351ba6db22dd204c
SalihSarang <sarangsalih@gmail.com>
2024-10-28 18:47:08 +0530

    Add files via upload

---
+++ sample/script.js
@@ -0,0 +1,7 @@
+let toggleLogo=document.querySelector(".toggle-logo");
+let menu = document.querySelector(".menu");
+
+toggleLogo.addEventListener("click", function(){
+    menu.classList.toggle("is-active");
+})
+    
