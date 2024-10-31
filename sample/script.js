1c38889eac41954f2ea0eeb8fb9e7bbacc959ca8
SalihSarang <sarangsalih@gmail.com>
2024-10-31 15:12:30 +0530

    Delete sample directory

--- sample/script.js
+++
@@ -1,7 +0,0 @@
-let toggleLogo=document.querySelector(".toggle-logo");
-let menu = document.querySelector(".menu");
-
-toggleLogo.addEventListener("click", function(){
-    menu.classList.toggle("is-active");
-})
-    
